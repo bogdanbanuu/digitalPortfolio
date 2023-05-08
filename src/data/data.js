567f5f25cbdad2b82833be111b670522eb9a7f26
Banu <bogdanbanu769@gmail.com>
2023-05-08 16:08:15 +0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import gericht from '../assets/projects/gericht.jpg'
+import robofriends from '../assets/projects/robofriends.jpg'
+import faceRecognition from '../assets/projects/facerecog.jpg'
+
+
+export const data=[
+    {
+        id:1,
+        name:"React Restaurant frontEnd-App",
+        image:gericht,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Robots jsonPlaceholders",
+        image:robofriends,
+        github:"",
+        live:"https://bogdanbanuu.github.io/robofriends/",
+    },
+    {
+        id:3,
+        name:"React JS faceRecognition clarifaiAPI",
+        image:faceRecognition,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:{},
+        github:"",
+        live:"",
+    },
+
+
+]
